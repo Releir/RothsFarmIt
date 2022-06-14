ed92dcec58aa255b089714e7277e3b30bbaf5445
Ron Sergio <sergio.ronj@gmail.com>
2022-06-14 15:24:06 +0800

    Added feature for debugging and usage for non command warp

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,11 +32,19 @@ namespace RothsFarmIt
             this.startBtn = new System.Windows.Forms.Button();
             this.stopBtn = new System.Windows.Forms.Button();
             this.frstLoginBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pathBox = new System.Windows.Forms.TextBox();
+            this.userNameBox = new System.Windows.Forms.TextBox();
+            this.passwordBox = new System.Windows.Forms.TextBox();
+            this.dbgAttach = new System.Windows.Forms.Button();
+            this.mapBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // startBtn
             // 
-            this.startBtn.Location = new System.Drawing.Point(38, 41);
+            this.startBtn.Location = new System.Drawing.Point(12, 140);
             this.startBtn.Name = "startBtn";
             this.startBtn.Size = new System.Drawing.Size(75, 23);
             this.startBtn.TabIndex = 0;
@@ -46,7 +54,7 @@ namespace RothsFarmIt
             // 
             // stopBtn
             // 
-            this.stopBtn.Location = new System.Drawing.Point(119, 41);
+            this.stopBtn.Location = new System.Drawing.Point(95, 140);
             this.stopBtn.Name = "stopBtn";
             this.stopBtn.Size = new System.Drawing.Size(75, 23);
             this.stopBtn.TabIndex = 1;
@@ -56,7 +64,7 @@ namespace RothsFarmIt
             // 
             // frstLoginBtn
             // 
-            this.frstLoginBtn.Location = new System.Drawing.Point(76, 70);
+            this.frstLoginBtn.Location = new System.Drawing.Point(12, 169);
             this.frstLoginBtn.Name = "frstLoginBtn";
             this.frstLoginBtn.Size = new System.Drawing.Size(75, 23);
             this.frstLoginBtn.TabIndex = 2;
@@ -64,17 +72,95 @@ namespace RothsFarmIt
             this.frstLoginBtn.UseVisualStyleBackColor = true;
             this.frstLoginBtn.Click += new System.EventHandler(this.frstLoginBtn_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Path :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Username :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Password :";
+            // 
+            // pathBox
+            // 
+            this.pathBox.Location = new System.Drawing.Point(62, 15);
+            this.pathBox.Name = "pathBox";
+            this.pathBox.Size = new System.Drawing.Size(231, 23);
+            this.pathBox.TabIndex = 6;
+            // 
+            // userNameBox
+            // 
+            this.userNameBox.Location = new System.Drawing.Point(90, 49);
+            this.userNameBox.Name = "userNameBox";
+            this.userNameBox.Size = new System.Drawing.Size(157, 23);
+            this.userNameBox.TabIndex = 7;
+            // 
+            // passwordBox
+            // 
+            this.passwordBox.Location = new System.Drawing.Point(90, 86);
+            this.passwordBox.Name = "passwordBox";
+            this.passwordBox.PasswordChar = '*';
+            this.passwordBox.Size = new System.Drawing.Size(157, 23);
+            this.passwordBox.TabIndex = 8;
+            // 
+            // dbgAttach
+            // 
+            this.dbgAttach.Location = new System.Drawing.Point(193, 140);
+            this.dbgAttach.Name = "dbgAttach";
+            this.dbgAttach.Size = new System.Drawing.Size(100, 23);
+            this.dbgAttach.TabIndex = 9;
+            this.dbgAttach.Text = "Debug Attach";
+            this.dbgAttach.UseVisualStyleBackColor = true;
+            this.dbgAttach.Click += new System.EventHandler(this.dbgAttach_Click);
+            // 
+            // mapBtn
+            // 
+            this.mapBtn.Location = new System.Drawing.Point(193, 169);
+            this.mapBtn.Name = "mapBtn";
+            this.mapBtn.Size = new System.Drawing.Size(100, 23);
+            this.mapBtn.TabIndex = 10;
+            this.mapBtn.Text = "Get Map";
+            this.mapBtn.UseVisualStyleBackColor = true;
+            this.mapBtn.Click += new System.EventHandler(this.mapBtn_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(240, 99);
+            this.ClientSize = new System.Drawing.Size(317, 228);
+            this.Controls.Add(this.mapBtn);
+            this.Controls.Add(this.dbgAttach);
+            this.Controls.Add(this.passwordBox);
+            this.Controls.Add(this.userNameBox);
+            this.Controls.Add(this.pathBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.frstLoginBtn);
             this.Controls.Add(this.stopBtn);
             this.Controls.Add(this.startBtn);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -83,6 +169,14 @@ namespace RothsFarmIt
         private System.Windows.Forms.Button startBtn;
         private System.Windows.Forms.Button stopBtn;
         private System.Windows.Forms.Button frstLoginBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox pathBox;
+        private System.Windows.Forms.TextBox userNameBox;
+        private System.Windows.Forms.TextBox passwordBox;
+        private System.Windows.Forms.Button dbgAttach;
+        private System.Windows.Forms.Button mapBtn;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Management;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
@@ -17,7 +18,34 @@ namespace RothsFarmIt
     {
         public Form1()
         {
+            CheckForIllegalCrossThreadCalls = false; //For the farm :'(
             InitializeComponent();
+        }
+
+
+        private static void KillProcessAndChildrens(int pid)
+        {
+            ManagementObjectSearcher processSearcher = new ManagementObjectSearcher
+              ("Select * From Win32_Process Where ParentProcessID=" + pid);
+            ManagementObjectCollection processCollection = processSearcher.Get();
+
+            try
+            {
+                Process proc = Process.GetProcessById(pid);
+                if (!proc.HasExited) proc.Kill();
+            }
+            catch (ArgumentException)
+            {
+                // Process already exited.
+            }
+
+            if (processCollection != null)
+            {
+                foreach (ManagementObject mo in processCollection)
+                {
+                    KillProcessAndChildrens(Convert.ToInt32(mo["ProcessID"])); //kill child processes(also kills childrens of childrens etc.)
+                }
+            }
         }
 
         //Important keys
@@ -34,13 +62,17 @@ namespace RothsFarmIt
         [DllImport("user32.dll")]
         static extern bool PostMessage(IntPtr hWnd, UInt32 Msg, int wParam, int lParam);
         const uint WM_KEYDOWN = 0x100;
+        const uint WM_SYSKEYDOWN = 0x0104;
         const uint WM_KEYUP = 0x101;
+        const uint WM_SYSKEYUP = 0x0105;
         const uint WM_CHAR = 0x102;
 
         [DllImport("user32.dll")]
         public static extern int SendMessage(IntPtr hWnd, UInt32 Msg, int wParam, int lParam);
         private const uint WM_LBUTTONDOWN = 0x0201;
         private const uint WM_LBUTTONUP = 0x0202;
+        private const uint WM_RBUTTONDOWN = 0x0204;
+        private const uint WM_RBUTTONUP = 0x0205;
 
         [DllImport("kernel32.dll")]
         public static extern Int32 ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress,
@@ -68,6 +100,15 @@ namespace RothsFarmIt
         private static int ReadInt32(IntPtr handle, long address)
         {
             return BitConverter.ToInt32(ReadBytes(handle, address, 4), 0);
+        }
+
+        static string parseMemoryMessage(Process proc, int addr)
+        {
+            byte[] buffer = ReadBytes(proc.Handle, addr, 100);
+            int firstNullIndex = Array.FindIndex(buffer, b => b == 0);
+            string msg = Encoding.Default.GetString(buffer, 0, firstNullIndex);
+            string[] msgContent = msg.Split(".");
+            return msgContent[0];
         }
 
         public static void pressKey(Process proc, Keys keybind)
@@ -102,7 +143,7 @@ namespace RothsFarmIt
                     int spAddr = hpAddr + 8;
                     int spMaxAddr = hpAddr + 12;
                     double spPercToPot = 10;
-                    double hpPercToPot = 80;
+                    double hpPercToPot = 1;
                     int spVal = 0;
                     int spMax = 0;
                     int hpVal = 0;
@@ -118,11 +159,11 @@ namespace RothsFarmIt
                         Thread.Sleep(100);
                         pressKey(proc, Keys.F7);
                     }
-                    if (hpPerc < hpPercToPot)
-                    {
-                        Thread.Sleep(100);
-                        pressKey(proc, Keys.F7);
-                    }
+                    //if (hpPerc < hpPercToPot)
+                    //{
+                    //    Thread.Sleep(100);
+                    //    pressKey(proc, Keys.F7);
+                    //}
                     if (token.IsCancellationRequested)
                     {
                         Console.WriteLine("Cancelled");
@@ -135,36 +176,40 @@ namespace RothsFarmIt
             }
         }
 
-
-
-
         public static void loginProcedure(Process proc)
         {
-            Thread.Sleep(8000);
+            Thread.Sleep(9000);
             pressKey(proc, Keys.Enter);
-            Thread.Sleep(1000);
+            Thread.Sleep(500);
             pressKey(proc, Keys.Tab);
-            Thread.Sleep(1000);
-            sendWords(proc, "*");
-            Thread.Sleep(1000);
+            Thread.Sleep(500);
+            sendWords(proc, "user here");
+            Thread.Sleep(500);
             pressKey(proc, Keys.Tab);
-            Thread.Sleep(1000);
-            sendWords(proc, "*");
-            Thread.Sleep(1000);
+            Thread.Sleep(500);
+            sendWords(proc, "Password here");
+            Thread.Sleep(500);
+            pressKey(proc, Keys.Enter);
+            Thread.Sleep(500);
+            pressKey(proc, Keys.Enter);
+            Thread.Sleep(500);
             pressKey(proc, Keys.Enter);
             Thread.Sleep(1000);
-            pressKey(proc, Keys.Enter);
-            Thread.Sleep(1000);
-            pressKey(proc, Keys.Enter);
-            Thread.Sleep(2000);
             pressKey(proc, Keys.F8);
-            Thread.Sleep(1000);
+            Thread.Sleep(500);
+            pressKey(proc, Keys.F8);
+            Thread.Sleep(1500);
         }
 
         public static void wizFarm(Process proc, CancellationToken token)
         {
             while (!token.IsCancellationRequested)
             {
+                int newCount = WinUtil.GetWindowCount(proc.Id);
+                if (newCount > 1)
+                {
+                    KillProcessAndChildrens(proc.Id);
+                }
                 if (token.IsCancellationRequested)
                 {
                     Console.WriteLine("Cancelled");
@@ -176,19 +221,150 @@ namespace RothsFarmIt
                     loginProcedure(proc);
                 } else
                 {
-                    IntPtr xAddr = (IntPtr)0x00E2EC74;
-                    IntPtr yAddr = (IntPtr)0x00E2EC78;
-                    pressKey(proc, Keys.F5);
-                    Thread.Sleep(50);
-                    pressKey(proc, Keys.F1);
-                    //New Era, Kratos IDS
-                    WriteBytes(proc.Handle, 0x00E2EC74, 509); //X
-                    WriteBytes(proc.Handle, 0x00E2EC78, 386); //Y
-                    Thread.Sleep(50);
-                    for (int i = 0; i < 5; i++)
+                    int mapAddr = 0x00E43F48;
+                    string mapName = parseMemoryMessage(proc, mapAddr);
+                    if(mapName == "ordeal_3-2")
                     {
-                        mouseClick(proc, (int)Keys.LButton);
+                        CastSkillAndTeleClip(proc);
+                    } else
+                    {
+                        WarpToGoldRoom(proc);
                     }
+                    
+                }
+            }
+        }
+
+        public static void WarpToGoldRoom(Process proc)
+        {
+            int mapAddr = 0x00E43F48;
+            string mapName = parseMemoryMessage(proc, mapAddr);
+            if (mapName == "prontera")
+            {
+                pressKey(proc, Keys.F7);
+                Thread.Sleep(800);
+                WriteBytes(proc.Handle, 0x00E2EC74, 515); //X
+                WriteBytes(proc.Handle, 0x00E2EC78, 356); //Y
+                Thread.Sleep(300);
+                mouseClick(proc, (int)Keys.LButton);
+                mouseClick(proc, (int)Keys.LButton);
+                Thread.Sleep(300);
+                pressKey(proc, Keys.Enter);
+                Thread.Sleep(300);
+                pressKey(proc, Keys.Enter);
+                Thread.Sleep(300);
+                pressKey(proc, Keys.Enter);
+                Thread.Sleep(300);
+                pressKey(proc, Keys.Enter);
+                Thread.Sleep(300);
+                pressKey(proc, Keys.Enter);
+                Thread.Sleep(300);
+                pressKey(proc, Keys.Enter);
+                Thread.Sleep(800);
+            }
+            
+        }
+
+        public static void CastSkillAndTeleClip(Process proc)
+        {
+            IntPtr xAddr = (IntPtr)0x00E2EC74;
+            IntPtr yAddr = (IntPtr)0x00E2EC78;
+            pressKey(proc, Keys.F9);
+            Thread.Sleep(100);
+            pressKey(proc, Keys.Enter);
+            Thread.Sleep(100);
+            //Thread.Sleep(3);
+            pressKey(proc, Keys.F1);
+            //New Era, Kratos IDS
+            WriteBytes(proc.Handle, 0x00E2EC74, 509); //X
+            WriteBytes(proc.Handle, 0x00E2EC78, 386); //Y
+                                                      //Thread.Sleep(3);
+            for (int i = 0; i < 6; i++)
+            {
+                pressKey(proc, Keys.F1);
+                Thread.Sleep(1);
+                mouseClick(proc, (int)Keys.LButton);
+            }
+            Thread.Sleep(100);
+        }
+
+        public static void CastSkillAndWarp(Process proc)
+        {
+            IntPtr xAddr = (IntPtr)0x00E2EC74;
+            IntPtr yAddr = (IntPtr)0x00E2EC78;
+            pressKey(proc, Keys.F9);
+            //Thread.Sleep(3);
+            pressKey(proc, Keys.F1);
+            //New Era, Kratos IDS
+            WriteBytes(proc.Handle, 0x00E2EC74, 509); //X
+            WriteBytes(proc.Handle, 0x00E2EC78, 386); //Y
+                                                      //Thread.Sleep(3);
+            for (int i = 0; i < 4; i++)
+            {
+                pressKey(proc, Keys.F1);
+                Thread.Sleep(1);
+                mouseClick(proc, (int)Keys.LButton);
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                pressKey(proc, Keys.F1);
+                Thread.Sleep(1);
+                mouseClick(proc, (int)Keys.LButton);
+            }
+            Thread.Sleep(1);
+        }
+
+        public static void autoStorage(Process proc, CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                int newCount = WinUtil.GetWindowCount(proc.Id);
+                if (newCount > 1)
+                {
+                    KillProcessAndChildrens(proc.Id);
+                }
+                if (token.IsCancellationRequested)
+                {
+                    Console.WriteLine("Cancelled");
+                }
+                if (proc.HasExited)
+                {
+                    proc.Start();
+                    Thread.Sleep(5000);
+                    loginProcedure(proc);
+                }
+                else
+                {
+                    int maxWeight = 0x010D94AC;
+                    int currWeight = 0x010D94B0;
+                    int maxWVal = ReadInt32(proc.Handle, maxWeight);
+                    int currWVal = ReadInt32(proc.Handle, currWeight);;
+                    double weightPerc = ((double)currWVal / (double)maxWVal) * 100;
+                    double WeightToStore = 60;
+
+                    if (weightPerc > WeightToStore) { 
+                        IntPtr xAddr = (IntPtr)0x00E2EC74;
+                        IntPtr yAddr = (IntPtr)0x00E2EC78;
+                        WriteBytes(proc.Handle, 0x00E2EC74, 54); //X
+                        WriteBytes(proc.Handle, 0x00E2EC78, 175); //Y
+
+                        SendMessage(proc.MainWindowHandle, WM_LBUTTONDOWN, (int)Keys.LButton, 0);
+                        Thread.Sleep(55);
+                        WriteBytes(proc.Handle, 0x00E2EC74, 344); //X
+                        WriteBytes(proc.Handle, 0x00E2EC78, 191); //Y
+                        Thread.Sleep(55);
+                        SendMessage(proc.MainWindowHandle, WM_LBUTTONUP, (int)Keys.LButton, 0);
+                        Thread.Sleep(55);
+                        pressKey(proc, Keys.Enter);
+                        Thread.Sleep(500);
+                    }
+                    //SendMessage(proc.MainWindowHandle, WM_SYSKEYDOWN, (int)Keys.Menu, 0);
+                    //Thread.Sleep(111);
+                    //SendMessage(proc.MainWindowHandle, WM_RBUTTONDOWN, (int)Keys.RButton, 0);
+                    //Thread.Sleep(111);
+                    //SendMessage(proc.MainWindowHandle, WM_RBUTTONUP, (int)Keys.RButton, 0);
+                    //Thread.Sleep(111);
+                    //SendMessage(proc.MainWindowHandle, WM_SYSKEYUP, (int)Keys.Menu, 0);
                 }
             }
         }
@@ -200,19 +376,21 @@ namespace RothsFarmIt
 
         private async void startBtn_Click(object sender, EventArgs e)
         {
+            startBtn.Enabled = false;
             if (ragnaProc.Container == null)
             {
                 src = new CancellationTokenSource();
                 CancellationToken ct = src.Token;
 
-                Task t1 = Task.Run(() => wizFarm(ragnaProc, ct), ct);
-                Task t2 = Task.Run(() => autoPots(ragnaProc, ct), ct);
+                var longOp1 = Task.Factory.StartNew(() => wizFarm(ragnaProc, ct), ct, TaskCreationOptions.LongRunning, TaskScheduler.Default);
+                //Task t1 = Task.Run(() => wizFarm(ragnaProc, ct), ct);
+                //Task t2 = Task.Run(() => autoPots(ragnaProc, ct), ct);
 
                 try
                 {
-                    await t1;
-                    await t2;
-                    //Task.WaitAny(t1, t2);
+                    await Task.WhenAll(new[] { longOp1 });
+                    //await t1;
+                    //await t2;
                 }
                 catch (AggregateException ae)
                 {
@@ -234,16 +412,38 @@ namespace RothsFarmIt
 
         private void stopBtn_Click(object sender, EventArgs e)
         {
-            src.Cancel();
+            try
+            {
+                src.Cancel();
+                startBtn.Enabled = true;
+            } catch (Exception exce)
+            {
+               
+            }
+            
         }
 
         private void frstLoginBtn_Click(object sender, EventArgs e)
         {
-            ragnaProc.StartInfo.FileName = "*";
+            ragnaProc.StartInfo.FileName = "File location here (use shortcut .lnk if protected)";
             ragnaProc.StartInfo.UseShellExecute = true;
             ragnaProc.Start();
 
             loginProcedure(ragnaProc);
+        }
+
+        private void dbgAttach_Click(object sender, EventArgs e)
+        {
+            Process[] proc = Process.GetProcessesByName("Process Name Here for debugging");
+            ragnaProc = proc[0];
+        }
+
+        private void mapBtn_Click(object sender, EventArgs e)
+        {
+            int mapAddr = 0x00E43F48;
+            string mapName = parseMemoryMessage(ragnaProc, mapAddr);
+            bool isPront = mapName == "prontera";
+            MessageBox.Show(isPront.ToString());
         }
     }
 }
